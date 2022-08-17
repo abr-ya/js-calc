f1d722cdddd44d7e6b1e5403987ea8369c95da00
abr-ya <gamer05@list.ru>
2022-08-17 07:37:00 +0300

    feat: Add display and keys elements

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,22 @@
+const options = '1234567890*/+-'.split('');
+const special = '*/+-'.split('');
+
 const init = () => {
   console.log('ready');
+  const root = document.querySelector('#root');
+
+  const container = document.createElement('div');
+  container.classList.add('container');
+  root.appendChild(container);
+
+  const display = document.createElement('input');
+  display.classList.add('display');
+  display.setAttribute('type', 'text');
+  container.appendChild(display);
+
+  const keys = document.createElement('div');
+  keys.classList.add('keys');
+  container.appendChild(keys);
 }
 
 window.addEventListener('DOMContentLoaded', init);
